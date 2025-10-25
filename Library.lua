f8685d338672ae0ad29f60498a1cd0214dd6fe1d
pppeke <u9011588954@gmail.com>
2025-10-25 09:23:57 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -41,7 +41,7 @@ local Library = {
 
     OpenedFrames = {};
     DependencyBoxes = {};
-
+a
     Signals = {};
     ScreenGui = ScreenGui;
 };
@@ -1063,7 +1063,7 @@ do
 
         local ModeSelectOuter = Library:Create('Frame', {
             BorderColor3 = Color3.new(0, 0, 0);
-            Position = UDim2.fromOffset(ToggleLabel.AbsolutePosition.X, ToggleLabel.AbsolutePosition.Y + 1);
+            Position = UDim2.fromOffset(ToggleLabel.AbsolutePosition.X + ToggleLabel.AbsoluteSize.X + 4, ToggleLabel.AbsolutePosition.Y + 1);
             Size = UDim2.new(0, 60, 0, 45 + 2);
             Visible = false;
             ZIndex = 14;
